3d73351eed7f8e0a2831eb83725bdec1a8e6758c
AnnaTykhomyrova <tykhomyrovaanna@gmail.com>
2019-07-02 22:15:25 +0000

    Handling User Actions

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -6,9 +6,11 @@ class ChatBar extends Component {
       <footer className="chatbar">
         <input className="chatbar-username" placeholder="Your Name (Optional)"
         defaultValue={this.props.currentUser.name} />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER"
+        onKeyPress={this.props.addMessage} />
       </footer>
     );
   }
 }
+
 export default ChatBar;
