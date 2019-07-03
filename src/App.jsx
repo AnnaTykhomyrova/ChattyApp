f71dd1a8b715da380f80d562cc1fc5030352693e
AnnaTykhomyrova <tykhomyrovaanna@gmail.com>
2019-07-03 16:39:01 +0000

    Connecting to a Websocket Server

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ import MessageList from "./MessageList.jsx";
 class App extends Component {
   constructor (props) {
     super(props);
+
+    // store the socket connection object
+    this.socket = new WebSocket('ws://localhost:3001');
     this.state = {
       currentUser: {name: "Bob"},
       messages: [
         {
-          id: 123,
+          id: 1,
           username: "Bob",
           content: "Has anyone seen my marbles?",
         },
         {
-          id: 456,
+          id: 2,
           username: "Anonymous",
           content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
         }
@@ -24,13 +27,15 @@ class App extends Component {
 
 componentDidMount() {
   console.log("componentDidMount <App />");
+  // const socket = new WebSocket("ws://localhost:3001/");
+  this.socket.onopen = function (event) {
+    console.log('Connected to server');
+  };
   setTimeout(() => {
     console.log("Simulating incoming message");
-
     // Add a new message to the list of messages in the data store
     const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-    const messages = this.state.messages.concat(newMessage);
-
+    const messages = this.state.messages.concat(newMessage)
     // Update the state of the app component.
     // Calling setState will trigger a call to render() in App and all child components.
     this.setState({messages: messages})
